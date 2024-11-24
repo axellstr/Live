34d381a153c8d074b07fc5bda6d1593a91b920e4
Alexandros Sotiriou <119623989+axellstr@users.noreply.github.com>
2024-11-24 13:14:20 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,6 +2,20 @@
 let basket = {};
 
 
+// API Functions
+function fetchStoreItems() {
+    return fetch("http://localhost:3000/store-items")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .catch(e => {
+            console.error("Could not fetch store items:", e.message);
+        });
+}
+
 
 // Utility functions for cart persistence
 function saveCartToLocalStorage() {
