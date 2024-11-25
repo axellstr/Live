0b31b74b1733546d27609a460def7f4e75da1ecf
Alexandros Sotiriou <119623989+axellstr@users.noreply.github.com>
2024-11-25 21:56:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -80,4 +80,5 @@ app.get('/store-items', (req, res) => {
   res.json(itemsArray);
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
