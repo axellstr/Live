e40266066700ef01ad6a68c37226b49d47f24d9f
Alexandros Sotiriou <119623989+axellstr@users.noreply.github.com>
2024-11-25 22:07:23 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -219,7 +219,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: item.id,
             quantity: item.quantity
         }));
-        fetch("/create-checkout-session", {
+        fetch("/api/create-checkout-session", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
